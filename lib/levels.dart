cd743a0cfada8dbcfbc401a6b3d415338b70246c
juanse217 <juansebas08004@gmail.com>
2025-06-05 17:46:24 -0500

    Funcionalidad play button, pantalla niveles

---
+++ lib/levels.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Levels extends StatelessWidget {
+  const Levels({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(),
+        brightness: Brightness.light,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                'Select Level',
+                textScaler: TextScaler.linear(3.5),
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 150),
+                width: 300,
+                height: 65,
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                  ),
+                  onPressed: () {},
+                  child: Text('EASY'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 100),
+                width: 300,
+                height: 65,
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    backgroundColor: WidgetStatePropertyAll(Colors.deepPurple),
+                  ),
+                  onPressed: () {},
+                  child: Text('MEDIUM'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 100),
+                width: 300,
+                height: 65,
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    backgroundColor: WidgetStatePropertyAll(Colors.amber),
+                  ),
+                  onPressed: () {},
+                  child: Text('HARD'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:color_match/levels.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,47 +7,55 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
-          brightness: Brightness.dark,
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Column(
-            children: [
-              Image.asset('assets/images/logo.GIF', width: 400.0),
-              Container(
-                margin: EdgeInsets.only(top: 100),
-                width: 200,
-                height: 50,
-                child: TextButton(
-                  style: ButtonStyle(
-                    foregroundColor: WidgetStateProperty.all<Color>(
-                      Colors.white,
-                    ),
-                    backgroundColor: WidgetStatePropertyAll(Colors.green),
-                    overlayColor: WidgetStateProperty.resolveWith<Color?>((
-                      Set<WidgetState> states,
-                    ) {
-                      if (states.contains(WidgetState.pressed))
-                        return Colors.black.withGreen(122);
-                      return null;
-                    }),
-                  ),
-                  onPressed: () => print("Hola"),
-                  child: Text("Play"),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(backgroundColor: Colors.black54),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/images/logo.GIF', width: 400.0),
+            Container(
+              margin: EdgeInsets.only(top: 100),
+              width: 200,
+              height: 50,
+              child: TextButton(
+                style: ButtonStyle(
+                  foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                  backgroundColor: WidgetStatePropertyAll(Colors.green),
+                  overlayColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed))
+                      return Colors.black.withGreen(122);
+                    return null;
+                  }),
                 ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Levels();
+                      },
+                    ),
+                  );
+                },
+                child: Text("Play"),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
