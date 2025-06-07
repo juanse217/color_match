f59a3a938930aaaf947c5719421fb575df44b524
jdvalmart <juanvalencia9411@outlook.com>
2025-06-06 23:03:27 -0500

    ajustes al main

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,165 +1,66 @@
-import 'package:color_match/main.dart';
-import 'package:color_match/levels.dart';
+import 'package:color_match/menu.dart';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
 
-
-class Menu extends StatelessWidget {
-  const Menu({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        home: Scaffold(
-            appBar: AppBar(
-                title:const Text(''),
-                leading:IconButton(
-                    icon:Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return HomeScreen();
-                          },
-                        ),
-                      );
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(backgroundColor: Colors.black54),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/images/logo.GIF', width: 400.0),
+            Container(
+              margin: EdgeInsets.only(top: 100),
+              width: 200,
+              height: 50,
+              child: TextButton(
+                style: ButtonStyle(
+                  foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                  backgroundColor: WidgetStatePropertyAll(Colors.green),
+                  overlayColor: WidgetStateProperty.resolveWith<Color?>((
+                      Set<WidgetState> states,
+                      ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Colors.black.withGreen(122);
                     }
-                )
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('MENU',textScaler: TextScaler.linear(3.5), style: TextStyle(fontWeight: FontWeight.bold),),
-                  Container(
-                    margin: EdgeInsets.only(top: 100),
-                    width: 300,
-                    height: 65,
-                    child: TextButton(
-                      style: ButtonStyle(
-                        foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                        backgroundColor: WidgetStatePropertyAll(Colors.blue),
-                        overlayColor: WidgetStateProperty.resolveWith<Color?>((
-                            Set<WidgetState> states,
-                            ) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return Colors.black.withGreen(122);
-                          }
-                          return null;
-                        }),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return Levels();
-                            },
-                          ),
-                        );
+                    return null;
+                  }),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Menu();
                       },
-                      child: Text("Jugar"),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 50),
-                    width: 300,
-                    height: 65,
-                    child: TextButton(
-                      style: ButtonStyle(
-                        foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                        backgroundColor: WidgetStatePropertyAll(Colors.deepPurple),
-                        overlayColor: WidgetStateProperty.resolveWith<Color?>((
-                            Set<WidgetState> states,
-                            ) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return Colors.black.withGreen(122);
-                          }
-                          return null;
-                        }),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return Levels();
-                            },
-                          ),
-                        );
-                      },
-                      child: Text("Desafio Semanal"),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 50),
-                    width: 300,
-                    height: 65,
-                    child: TextButton(
-                      style: ButtonStyle(
-                        foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                        backgroundColor: WidgetStatePropertyAll(Colors.deepOrange),
-                        overlayColor: WidgetStateProperty.resolveWith<Color?>((
-                            Set<WidgetState> states,
-                            ) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return Colors.black.withGreen(122);
-                          }
-                          return null;
-                        }),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return Levels();
-                            },
-                          ),
-                        );
-                      },
-                      child: Text("Tabla de clasificación"),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 50),
-                    width: 300,
-                    height: 65,
-                    child: TextButton(
-                      style: ButtonStyle(
-                        foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                        backgroundColor: WidgetStatePropertyAll(Colors.amber),
-                        overlayColor: WidgetStateProperty.resolveWith<Color?>((
-                            Set<WidgetState> states,
-                            ) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return Colors.black.withGreen(122);
-                          }
-                          return null;
-                        }),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return Levels();
-                            },
-                          ),
-                        );
-                      },
-                      child: Text("Configuración"),
-                    ),
-                  ),
-
-                ],
-
+                  );
+                },
+                child: Text("Empezar"),
               ),
-            )
-        )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
